370ee4b8bac9f8f098ddf416cc62f132a80eb8c9
Xiuming Liu <xiuming.liu@it.uu.se>
2019-02-04 08:40:07 +0100

    add 2 stage regression

--- example/example_synthetic_data.py
+++ example/example_synthetic_data.py
@@ -29,9 +29,10 @@ q = 1    # dimension of z
 
 # Covariance matrices
 Cov_xzy = np.array([[1, 0, .3, .5],
-                    [0, 1, .3, .6],
-                    [.3, .3, 1, .7],
-                    [.5, .6, .7, 1]])
+                    [0, 1, .3, .5],
+                    [.3, .3, 1, .5],
+                    [.5, .5, .5, 1]])
+LA.eig(Cov_xzy)
 plt.figure()
 sns.heatmap(Cov_xzy)
 
@@ -51,8 +52,8 @@ Cov_xy = Cov_xzy[0:d, -1].reshape(d, 1)
 # s.t Aw = b
 # =============================================================================
 
-Q = matrix(Cov_xx)
-p = matrix(-2*Cov_xy)
+Q = 2*matrix(Cov_xx)
+p = -2*matrix(Cov_xy)
 A = matrix(Gamma)
 b = matrix(np.dot(Gamma,alpha) + beta)
 
@@ -68,11 +69,19 @@ w_const = np.array(w_const)
 # min w'Qw + p'w
 # =============================================================================
 
-Q = matrix(Cov_xx)
-p = matrix(-2*Cov_xy)
+#Q = 2*matrix(Cov_xx)
+#p = -2*matrix(Cov_xy)
+#
+#w_unconst = qp(Q, p)['x']
+#w_unconst = np.array(w_unconst)
 
-w_unconst = qp(Q, p)['x']
-w_unconst = np.array(w_unconst)
+w_unconst = np.dot(inv(Cov_xx), Cov_xy)
+
+# =============================================================================
+# Predict z first, then y  
+# =============================================================================
+
+Theta = np.transpose(np.dot(Cov_xzy[d:d+q, 0:d], inv(Cov_xzy[0:d, 0:d])))
 
 # =============================================================================
 # Testing
@@ -81,9 +90,10 @@ w_unconst = np.array(w_unconst)
 # Generate testing data
 # (x, y, z) ~ N(mu, Cov)
 Cov_xyz = np.array([[1, 0, .5, .3,],
-                    [0, 1, .6, .3,],
-                    [.5, .6, 1, .7],
-                    [.3, .3, .7, 1]])
+                    [0, 1, .5, .3,],
+                    [.5, .5, 1, .5],
+                    [.3, .3, .5, 1]])
+LA.eig(Cov_xyz)
 plt.figure()
 sns.heatmap(Cov_xyz)
 
@@ -95,14 +105,37 @@ def conditional_mean(z, mu_z, mu_xy, Cov_xyz):
 
 #mu_xy_z, Cov_xy_z = conditional_mean(1, 0, 0, Cov_xyz)
 
-for z in range(-5, 5, 1):
+MSE_const = np.zeros((105, 1))
+MSE_unconst = np.zeros((105, 1))
+MSE_2stages = np.zeros((105, 1))
+for j in range(0, 105, 1):
+    z = -10 + j*.2
     mu_xy_z, Cov_xy_z = conditional_mean(z, 0, 0, Cov_xyz)
-    y_true = np.zeros((100,1))
-    y_predict_const = np.zeros((100,1))
-    y_predict_unconst = np.zeros((100,1))
-    for i in range(0, 100):
+    y_true = np.zeros((5000,1))
+    y_predict_const = np.zeros((5000,1))
+    y_predict_unconst = np.zeros((5000,1))
+    z_predict_2stage = np.zeros((5000,1))
+    y_predict_2stage = np.zeros((5000,1))
+    for i in range(0, 5000):
         xy = np.random.multivariate_normal(mu_xy_z.reshape(d+1), Cov_xy_z)
         y_true[i] = xy[d:]
         y_predict_const[i] = np.dot(np.transpose(w_const), xy[0:d].reshape(d,1))
         y_predict_unconst[i] = np.dot(np.transpose(w_unconst), xy[0:d].reshape(d,1))
         
+        z_predict_2stage[i] = np.dot(np.transpose(Theta), xy[0:d].reshape(d,1))
+        y_predict_2stage[i] = np.dot(alpha_beta_joint, xy[0:d+q].reshape(d+q,1))
+        
+    MSE_const[j] = mean_squared_error(y_true, y_predict_const) 
+    MSE_unconst[j] = mean_squared_error(y_true, y_predict_unconst) 
+    MSE_2stages[j] = mean_squared_error(y_true, y_predict_2stage) 
+    
+plt.figure()
+sns.regplot(np.arange(-10, 11, .2), MSE_const.reshape(105), order=3, label="Constrained")
+sns.regplot(np.arange(-10, 11, .2), MSE_unconst.reshape(105), order=3, label="Unconstrained")  
+sns.regplot(np.arange(-10, 11, .2), MSE_2stages.reshape(105), order=3, label="2 stages") 
+plt.xlim((-11, 11))
+plt.ylim((0, 10))
+plt.legend()
+#plt.legend("Constrained", "Unconstrained")      
+plt.xlabel("z")
+plt.ylabel("MSE(z)")
